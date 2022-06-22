8559a2710fe1b08de78c8c1800c5fa12ff54d9f1
CraXWaR <craxwar@gmail.com>
2022-06-22 15:47:05 +0300

    Exam march prep

--- JS Advanced Exam - 13 March 2022/carDealership.js
+++ JS Advanced Exam - 13 March 2022/carDealership.js
@@ -1,3 +1,81 @@
 class CarDealership {
+    constructor(name) {
+        this.name = name
+        this.availableCars = []
+        this.soldCars = []
+        this.totalIncome = 0
+    }
+    addCar(model, horsepower, price, mileage) {
+        if (model == '' || horsepower < 0 || price < 0 || mileage < 0) {
+            throw new Error("Invalid input!")
+        }
+        this.availableCars.push({
+            model,
+            horsepower,
+            price,
+            mileage
+        });
+        return `New car added: ${model} - ${horsepower} HP - ${mileage.toFixed(2)} km - ${price.toFixed(2)}$`
+    }
+    sellCar(model, desiredMileage) {
+        let soldPrice = 0
+        let carIndex = this.availableCars.findIndex(c => c.model == model);
+        if(carIndex == -1){
+            throw Error(`${model} was not found!`);
+        } 
+        if(this.availableCars[carIndex].mileage > desiredMileage){
+            let diff = this.availableCars[carIndex].mileage - desiredMileage
+            if (diff <= 40000){
+                soldPrice = this.availableCars[carIndex].price - (this.availableCars[carIndex].price * 0.05);
+            } else if (diff > 40000){
+                soldPrice = this.availableCars[carIndex].price - (this.availableCars[carIndex].price * 0.10);
+            }
+        } else {
+            soldPrice = this.availableCars[carIndex].price;
+        }
+        this.soldCars.push(
+            {
+                model: this.availableCars[carIndex].model,
+                horsepower: this.availableCars[carIndex].horsepower,
+                soldPrice: soldPrice
+            }
+        );
+        this.totalIncome += soldPrice;
+        this.availableCars.slice(carIndex, 1);
+ 
+        return `${model} was sold for ${soldPrice.toFixed(2)}$`;
+    }
+    currentCar() {
+        if (this.availableCars.length == 0) {
+            return "There are no available cars"
+        }
+        let res = ["-Available cars:"]
+        for (let c of this.availableCars) {
+            res.push(`---${c.model} - ${c.horsepower} HP - ${c.mileage.toFixed(2)} km - ${c.price.toFixed(2)}$`);
+        }
+        return res.join('\n')
+    }
+    salesReport(criteria) {
+        let sortedCars
+        if (criteria == 'horsepower') {
+            sortedCars = this.soldCars.sort((a, b) => b.horsepower - a.horsepower)
+        } else if (criteria == 'model') {
+            sortedCars = this.soldCars.sort((a, b) => (a.model).localeCompare(b.model))
+        } else {
+            throw Error('Invalid criteria!');
+        }
 
+        let result = [`-${this.name} has a total income of ${this.totalIncome.toFixed(2)}$`, `-${this.soldCars.length} cars sold:`];
+        for (const c of sortedCars) {
+            result.push(`---${c.model} - ${c.horsepower} HP - ${c.soldPrice.toFixed(2)}$`)
+        }
+        return result.join('\n')
+    }
 }
+let dealership = new CarDealership('SoftAuto');
+dealership.addCar('Toyota Corolla', 100, 3500, 190000);
+dealership.addCar('Mercedes C63', 300, 29000, 187000);
+dealership.addCar('Audi A3', 120, 4900, 240000);
+dealership.sellCar('Toyota Corolla', 230000);
+dealership.sellCar('Mercedes C63', 110000);
+console.log(dealership.salesReport('horsepower'));
---
+++ JS Advanced Exam - 13 March 2022/test/rentCar.test.js
@@ -0,0 +1,49 @@
+const {expect} = require('chai')
+const {rentCar} = require('../rentCar')
+
+describe("rentCar", () => {
+    describe("searchCar", () => {
+        it("test1", () => {
+            expect(() => rentCar.searchCar({}, 'BMW')).to.throw("Invalid input!")
+            expect(() => rentCar.searchCar('AUDI', 1)).to.throw("Invalid input!")
+            expect(() => rentCar.searchCar(1, 'BMW')).to.throw("Invalid input!")
+            expect(() => rentCar.searchCar('AUDI', 'BMW')).to.throw("Invalid input!")
+            expect(() => rentCar.searchCar(['AUDI', 'Volkswagen', 'Toyota'], 'BMW')).to.throw("There are no such models in the catalog!")
+            expect(() => rentCar.searchCar([], 'BMW')).to.throw("There are no such models in the catalog!")
+            expect(rentCar.searchCar(['AUDI', 'Volkswagen', 'Toyota', 'BMW'], 'BMW')).to.be.equal(`There is 1 car of model BMW in the catalog!`)
+        });
+    })
+    describe('calculatePriceOfCar', () => {
+        it("test2", () => {
+            expect(() => rentCar.calculatePriceOfCar('BMW', '1')).to.throw("Invalid input!")
+            expect(() => rentCar.calculatePriceOfCar('BMW', [])).to.throw("Invalid input!")
+            expect(() => rentCar.calculatePriceOfCar('BMW', {})).to.throw("Invalid input!")
+            expect(() => rentCar.calculatePriceOfCar('BMW', null)).to.throw("Invalid input!")
+            expect(() => rentCar.calculatePriceOfCar(5, null)).to.throw("Invalid input!")
+            expect(() => rentCar.calculatePriceOfCar([], 1)).to.throw("Invalid input!")
+            expect(() => rentCar.calculatePriceOfCar({}, 1)).to.throw("Invalid input!")
+            expect(() => rentCar.calculatePriceOfCar(2, 1)).to.throw("Invalid input!")
+            expect(() => rentCar.calculatePriceOfCar(undefined, 1)).to.throw("Invalid input!")
+            expect(() => rentCar.calculatePriceOfCar('Opel', 1)).to.throw("No such model in the catalog!")
+            expect(() => rentCar.calculatePriceOfCar('abgfa', 1)).to.throw("No such model in the catalog!")
+            expect(rentCar.calculatePriceOfCar('BMW', 1)).to.be.equal(`You choose BMW and it will cost $45!`)
+            expect(rentCar.calculatePriceOfCar('Mercedes', 2)).to.be.equal(`You choose Mercedes and it will cost $100!`)
+        });
+    })
+    describe('checkBudget', () => {
+        it("test3", () => {
+            expect(() => rentCar.checkBudget(1, 2, '3')).to.throw("Invalid input!")
+            expect(() => rentCar.checkBudget(1, '2', '3')).to.throw("Invalid input!")
+            expect(() => rentCar.checkBudget('1', '2', '3')).to.throw("Invalid input!")
+            expect(() => rentCar.checkBudget(1, 2, null)).to.throw("Invalid input!")
+            expect(() => rentCar.checkBudget(1, 2, '3')).to.throw("Invalid input!")
+            expect(() => rentCar.checkBudget(1, [], 3)).to.throw("Invalid input!")
+            expect(() => rentCar.checkBudget({}, 2, 3)).to.throw("Invalid input!")
+            expect(rentCar.checkBudget(1, 2, 3)).to.be.equal(`You rent a car!`)
+            expect(rentCar.checkBudget(1, 2, 100)).to.be.equal(`You rent a car!`)
+            expect(rentCar.checkBudget(2, 2, 4)).to.be.equal(`You rent a car!`)
+            expect(rentCar.checkBudget(10, 2, 3)).to.be.equal(`You need a bigger budget!`)
+            expect(rentCar.checkBudget(10, 2, 2)).to.be.equal(`You need a bigger budget!`)
+        });
+    })
+})
